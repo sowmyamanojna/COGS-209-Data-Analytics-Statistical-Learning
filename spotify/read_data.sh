7dca5b5234ff018e72983aa0f76b196c63865075
sowmyamanojna <sowmyamanojna@gmail.com>
2023-04-14 17:22:18 -0700

    Final data and code for Proposal

--- spotify/read_data.sh
+++ spotify/read_data.sh
@@ -1,6 +1,3 @@
-# curl "https://api.spotify.com/v1/playlists/6tsGGO4rewMHMjbvSUHHc7"      -H "Authorization: Bearer  BQASjNQqBiFPaOo-_amkMnhINnEZT6QLcDXm1GUr4QkFy-gBuO8jxeB9384FxLBhk6MFkLizdK18JWkLeowPnv_V4fNMOI2zgAiXHzmg-oXvOZF53BSB"
-# curl "https://api.spotify.com/v1/audio-features/0e8nrvls4Qqv5Rfa2UhqmO"      -H "Authorization: Bearer  BQASjNQqBiFPaOo-_amkMnhINnEZT6QLcDXm1GUr4QkFy-gBuO8jxeB9384FxLBhk6MFkLizdK18JWkLeowPnv_V4fNMOI2zgAiXHzmg-oXvOZF53BSB"
-
 curl "https://api.spotify.com/v1/playlists/6tsGGO4rewMHMjbvSUHHc7"      -H "Authorization: Bearer  BQASjNQqBiFPaOo-_amkMnhINnEZT6QLcDXm1GUr4QkFy-gBuO8jxeB9384FxLBhk6MFkLizdK18JWkLeowPnv_V4fNMOI2zgAiXHzmg-oXvOZF53BSB" --output 2022.json
 curl "https://api.spotify.com/v1/playlists/1bjAgktyYGqKJXynU4H8SO"      -H "Authorization: Bearer  BQASjNQqBiFPaOo-_amkMnhINnEZT6QLcDXm1GUr4QkFy-gBuO8jxeB9384FxLBhk6MFkLizdK18JWkLeowPnv_V4fNMOI2zgAiXHzmg-oXvOZF53BSB" --output 2021.json
 curl "https://api.spotify.com/v1/playlists/1WBljFutuk7uLQtfqfmjWV"      -H "Authorization: Bearer  BQASjNQqBiFPaOo-_amkMnhINnEZT6QLcDXm1GUr4QkFy-gBuO8jxeB9384FxLBhk6MFkLizdK18JWkLeowPnv_V4fNMOI2zgAiXHzmg-oXvOZF53BSB" --output 2020.json
